b61841e53aa9368458bec5963bc0438daa8a280c
lbo <lbo@spheniscida.de>
2013-09-13 14:42:15 +0200

    Started converting SymTerm type to type class.

--- Symmath/Terms.hs
+++ Symmath/Terms.hs
@@ -1,10 +1,13 @@
+{-# LANGUAGE GADTs #-}
+
 module Symmath.Terms where
+
 
 import Data.Char (toLower)
 import Symmath.Constants
 
 -- Data types
-
+{-
 data SymTerm = Number Double
              | Variable Char
              | Constant Constant
@@ -21,46 +24,84 @@ data SymTerm = Number Double
              | Signum SymTerm
              | UndefP Double SymTerm
             deriving Eq
+-}
 
-data Constant = Euler | Pi | Phi deriving (Eq, Show)
+data Number = Number Double deriving Eq
+data Variable = Variable Char deriving Eq
+data Constant = Euler | Pi | Phi deriving Show
+data Product where Product :: (SymTerm a, SymTerm b) => a -> b -> Product
+data Difference where Difference :: (SymTerm a, SymTerm b) => a -> b -> Difference
+data Sum where Sum :: (SymTerm a, SymTerm b) => a -> b -> Sum
+data Power where Power :: (SymTerm a, SymTerm b) => a -> b -> Power
+data Exp where Exp :: (SymTerm a) => a -> Exp
+data Trigo where Trigo :: (SymTerm b) => TrigoFun -> b -> Trigo
+data Ln where Ln :: (SymTerm a) => a -> Ln
+data Log where Log :: (SymTerm a, SymTerm b) => a -> b -> Log
+data Abs where Abs :: (SymTerm a) => a -> Abs
+data Signum where Signum :: (SymTerm a) => a -> Signum
+data UndefP where UndefP :: (SymTerm b) => Double -> b -> UndefP
 
-data Trigo = Sin | Cos | Tan deriving (Eq, Show)
+data TrigoFun = Sin | Cos | Tan deriving (Eq, Show)
 
--- Instances
 
-instance Show SymTerm where
+-- Type classes and instances
+class (Show a) => SymTerm a where
+
+instance SymTerm Number where
+
+instance SymTerm Variable where
+
+instance SymTerm Constant where
+
+instance SymTerm Product where
+
+instance SymTerm Difference where
+
+instance SymTerm Sum where
+
+instance SymTerm Power where
+
+instance SymTerm Exp where
+
+instance SymTerm Trigo where
+
+instance SymTerm Ln where
+
+instance SymTerm Log where
+
+instance SymTerm Abs where
+
+instance SymTerm Signum where
+
+instance SymTerm UndefP where
+
+
+--- Show instances
+
+instance Show Number where
     show (Number n) = show n
+instance Show Variable where
     show (Variable v) = [v]
-    show (Constant c) = show c
-    show (Product (Number (-1)) term2) = "(-" ++ (show term2) ++ ")"
-    show (Product term1 term2) = (show term1) ++ " * " ++ (show term2)
-    show (Sum term1 (Product (Number (-1)) term2)) = '(' : (show term1) ++ " - " ++ (show term2) ++ ")"
-    show (Sum term1 term2) = '(' : (show term1) ++ " + " ++ (show term2) ++ ")"
-    show (Difference term1 term2) = '(' : (show term1) ++ " - " ++ (show term2) ++ ")"
-    show (Fraction term1 term2) = '(' : (show term1) ++ " / " ++ (show term2) ++ ")"
-    show (Power term1 term2) = '(' : (show term1) ++ ")^(" ++ (show term2) ++ ")"
-    show (Exp term1) = "exp(" ++ show term1 ++ ")"
-    show (Ln term1) = "ln(" ++ show term1 ++ ")"
-    show (Log base term) = "log(" ++ show base ++ "," ++ show term ++ ")"
-    show (Abs term) = '|' : show term ++ "|"
-    show (Signum term) = "sgn(" ++ show term ++ ")"
-    show (Trigo trigo term1) = (map toLower $ show trigo) ++ "(" ++ show term1 ++ ")"
+instance Show Product where
+    show (Product t1 t2) = '(' : show t1 ++ " * " ++ show t2 ++ ")"
+instance Show Difference where
+    show (Difference t1 t2) = '(' : show t1 ++ " - " ++ show t2 ++ ")"
+instance Show Sum where
+    show (Sum t1 t2) = '(' : show t1 ++ " + " ++ show t2 ++ ")"
+instance Show Power where
+    show (Power t1 t2) = '(' : show t1 ++ ")^(" ++ show t2 ++ ")"
+instance Show Exp where
+    show (Exp t) = "exp(" ++ show t ++ ")"
+instance Show Trigo where
+    show (Trigo f t) = (map toLower . show $ f) ++ "(" ++ show t ++ ")"
+instance Show Ln where
+    show (Ln t) = "ln(" ++ show t ++ ")"
+instance Show Log where
+    show (Log b t) = "log(" ++ show b ++ "," ++ show t ++ ")"
+instance Show Abs where
+    show (Abs t) = '|' : show t ++ "|"
+instance Show Signum where
+    show (Signum t) = "sgn(" ++ show t ++ ")"
+instance Show UndefP where
+    show (UndefP p t) = "[" ++ show t ++ "| undefined at " ++ show p ++ "]"
 
-instance Num SymTerm where
-    (+) = Sum
-    (*) = Product
-    (-) = Difference
-    negate = Product (Number (-1))
-    abs = Abs
-    fromInteger = Number . fromIntegral
-    signum = Signum
-
-instance Fractional SymTerm where
-    t1 / t2 = Fraction t1 t2
-    fromRational = Number . fromRational
-
--- Helper
-constToNumber :: Constant -> Double
-constToNumber Euler = euler
-constToNumber Pi = pi
-constToNumber Phi = phi
