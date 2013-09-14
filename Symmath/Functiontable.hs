07526676a9d2750aa58883e2243e042a09af4069
lbo <lbo@spheniscida.de>
2013-09-14 17:15:24 +0200

    Beta: Function value table.
    TODO:
        - Align the table as Perl would do it with its formats.
        - Allow rounding to more or less decimals than 3

--- Symmath/Functiontable.hs
+++ Symmath/Functiontable.hs
@@ -3,6 +3,7 @@ module Symmath.Functiontable where
 import Data.Maybe
 import Control.Monad
 import Data.List
+import Text.Printf
 
 import Text.PrettyPrint.Boxes
 
@@ -12,25 +13,48 @@ import Symmath.Util
 
 data FuncValue = Value Double | Undefined
 
+
 instance Show FuncValue where
     show (Value n) = show n
     show Undefined = "undef"
 
 calcFunc :: SymTerm -> Double -> Double -> Double -> [(Double,FuncValue)]
-calcFunc t from to interval = foldr (\x l -> (x,funcAtPt x):l) [] [from,from+interval..to]
-        where funcAtPt x = let fx = evalTermP t [('x',x)] in
+calcFunc = calcFuncInd 'x'
+
+calcFuncInd :: Char -> SymTerm -> Double -> Double -> Double -> [(Double,FuncValue)]
+calcFuncInd indep t from to interval = foldr (\x l -> (x,funcAtPt x):l) [] [from,from+interval..to]
+        where funcAtPt x = let fx = evalTermP t [(indep,x)] in
                                 if fx == Nothing
                                 then Undefined
                                 else Value . fromMb $ fx
 
 printCalcFunc :: SymTerm -> Double -> Double -> Double -> Box
-printCalcFunc t from to interval = vcat top $ columnTitle:values
+printCalcFunc = printCalcFuncInd 'x'
+
+printCalcFuncInd :: Char -> SymTerm -> Double -> Double -> Double -> Box
+printCalcFuncInd indep t from to interval = vcat top $ columnTitle:values
         where termString = show t
-              columnTitle = text "x" <> moveRight 5 (text termString) `v0cat`  text (replicate (15 + length termString) '-')
-              values = map doubleToBox $ calcFunc t from to interval
-              doubleToBox (x,v) = text (show x) <+> moveRight 5 (text . show $ v)
+              columnTitle = char indep <> moveRight 10 (text termString) `v0cat`  text (replicate (15 + length termString) '-')
+              values = map doubleToBox $ calcFuncInd indep t from to interval
+              doubleToBox (x,v) = text (printf "%.3f" x) <+> moveRight 5 (text . printfFuncValue $ v)
+
+printCalcFuncs :: [SymTerm] -> Double -> Double -> Double -> Box
+printCalcFuncs = printCalcFuncsIndSp 'x' 5
+
+printCalcFuncsIndSp :: Char -> Int -> [SymTerm] -> Double -> Double -> Double -> Box
+printCalcFuncsIndSp indep space ts from to interval = vcat top [titles,separator,columns]
+        where titles = char indep <> foldr (\term box -> moveRight (space+2) (text . ('(':) . (++")") . show $ term) <> box) nullBox ts
+              separator = text $ replicate (space + 3 + (length . render $ titles)) '-'
+              columns = let funcvals = map (\t -> calcFuncInd indep t from to interval) ts in
+                        indepsToColumn <> (moveRight space $ foldr (\vals box -> valsToColumn vals <> (moveRight space box)) nullBox funcvals)
+              valsToColumn = foldr (\(x,fx) box -> (text . printfFuncValue $ fx) // box) nullBox
+              indepsToColumn = foldr (\x box -> (text . printf "%.3f" $ x) // box) nullBox [from,from+interval..to]
 
 -- Util
 
 v0cat :: Box -> Box -> Box
 v0cat a b = vcat top [a,b]
+
+printfFuncValue :: FuncValue -> String
+printfFuncValue Undefined = "undef"
+printfFuncValue (Value n) = printf "%.3f" n
