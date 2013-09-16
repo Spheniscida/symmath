636aa05fd705e6a1df74f7a1ed91dd053722ee14
lbo <lbo@spheniscida.de>
2013-09-16 19:36:43 +0200

    Fixed some tests which failed because of syntax changes.
    (7 + x) /= (x + 7)...

--- Test/TestSimplify.hs
+++ Test/TestSimplify.hs
@@ -24,6 +24,8 @@ tests = TestList [
         , testProduct9
         , testProduct10
         , testProduct11
+        , testProduct12
+        , testProduct13
 
         , testNumberDiff1
 
@@ -33,10 +35,10 @@ tests = TestList [
       ]
 
 testNumberSum1 = TestCase $ assertEqual "3 + 4 == 7" (Number 7) (simplify ((Number 3) + (Number 4)))
-testNumberSum2 = TestCase $ assertEqual "(x + 3) + 4 == x + 7" (Sum x (Number 7)) (simplify $ (x + (Number 3)) + (Number 4))
-testNumberSum3 = TestCase $ assertEqual "(3 + x) + 4 == x + 7" (Sum x (Number 7)) (simplify $ ((Number 3) + x) + (Number 4))
-testNumberSum4 = TestCase $ assertEqual "3 + (x + 4) == x + 7" (Sum x (Number 7)) (simplify $ (Number 3) + (Sum x (Number 4)))
-testNumberSum5 = TestCase $ assertEqual "3 + (4 + x) == x + 7" (Sum x (Number 7)) (simplify $ (Number 3) + (Sum (Number 4) x))
+testNumberSum2 = TestCase $ assertEqual "(x + 3) + 4 == x + 7" ((Number 7) + x) (simplify $ (x + (Number 3)) + (Number 4))
+testNumberSum3 = TestCase $ assertEqual "(3 + x) + 4 == x + 7" ((Number 7) + x) (simplify $ ((Number 3) + x) + (Number 4))
+testNumberSum4 = TestCase $ assertEqual "3 + (x + 4) == x + 7" ((Number 7) + x) (simplify $ (Number 3) + (Sum x (Number 4)))
+testNumberSum5 = TestCase $ assertEqual "3 + (4 + x) == x + 7" ((Number 7) + x) (simplify $ (Number 3) + (Sum (Number 4) x))
 
 testNumberProduct1 = TestCase $ assertEqual "3 * 4 == 12" (Number 12) (simplify $ (Number 3) * (Number 4))
 testProduct2 = TestCase $ assertEqual "x * y == x * y" (x * y) (simplify $ x * y)
@@ -46,10 +48,11 @@ testProduct5 = TestCase $ assertEqual "x * x == x^2" (Power x (Number 2)) (simpl
 testProduct6 = TestCase $ assertEqual "x^2 * x == x^3" (Power x (Number 3)) (simplify $ (Power x (Number 2)) * x)
 testProduct7 = TestCase $ assertEqual "x * x * x == x^3" (Power x (Number 3)) (simplify $ x * x * x)
 testProduct8 = TestCase $ assertEqual "0 * x == 0" (Number 0) (simplify $ x * (Number 0))
-testProduct9 = TestCase $ assertEqual "x * 0 == 0" (Number 0) (simplify $ (Number 0) * x)
+testProduct9 = TestCase $ assertEqual "x * 0 == 0" (Number 0) (simplify $ x * (Number 0))
 testProduct10 = TestCase $ assertEqual "(1+2) * (2+3) == 15" (Number 15) (simplify $ ((Number 1) + (Number 2)) * ((Number 2) + (Number 3)))
 testProduct11 = TestCase $ assertEqual "(y * x) * y == x * y^2" (x * (Power y (Number 2))) (simplify $ x * y * y)
-testProduct12 = TestCase $ assertEqual "(y * x) * y * (z * y * x * y) == y^4 * x^2 * z" (x * (Power y (Number 2))) (simplify $ x * y * y)
+testProduct12 = TestCase $ assertEqual "(y * x) * y * (z * y * x * y) == x^2 * y^4 * z" ((Power x (Number 2)) * ((Power y (Number 4)) * z)) (simplify $ y * x * y * z * y * x * y)
+testProduct13 = TestCase $ assertEqual "y * x * y^(-1) == x" x (simplify $ x * (Power y (Number (-1))) * y)
 
 testNumberDiff1 = TestCase $ assertEqual "3 - 4 == -1" (Number (-1)) (simplify (Difference (Number 3) (Number 4)))
 
