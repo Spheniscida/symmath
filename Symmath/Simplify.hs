8a432a9951adc6a52f4f093b8fddd6c166dae863
lbo <lbo@spheniscida.de>
2013-09-16 18:22:14 +0200

    Simplification of sums improved.
    e.g.
    
    ghci> simplify $ x + z + 2*y + x + 3 * z
    (2.0 * x + (2.0 * y + 4.0 * z))
    
    (This version does not factor out numbers anymore)

--- Symmath/Simplify.hs
+++ Symmath/Simplify.hs
@@ -39,8 +39,9 @@ simplifySum (Sum t1 (Product (Number (-1)) t2)) | t1 == t2 = Number 0
 simplifySum (Sum t1@(Product _ _) t2@(Product _ _)) = case prodListIntersectTuple (prodToList t1) (prodToList t2) of
                                                         ([],rest1,rest2) -> Sum (listToProd $ rest1) (listToProd $ rest2)
                                                         (common,rest1,rest2) -> Product (listToProd common) (Sum (Product (Number 1) (listToProd rest1)) (Product (Number 1) (listToProd rest2)))
-simplifySum (Sum t1 t2) | t1 == t2 = Product (Number 2) t1
-                        | otherwise = cleanSum $ Sum (simplifyOnce t1) (simplifyOnce t2)
+simplifySum (Sum t1 t2) = cleanSum $ Sum (simplifyOnce t1) (simplifyOnce t2)
+
+
 -- Products
 simplifyProd :: SymTerm -> SymTerm
 -- Plain numbers and terms
@@ -50,7 +51,7 @@ simplifyProd (Product (Number 0) _term) = Number 0
 simplifyProd (Product _term (Number 0)) = Number 0
 -- a * b => c (c == a * b)
 simplifyProd (Product (Number n1) (Number n2)) = Number $ n1 * n2
--- Simplify negative powers - this doesn't work with the current algos in cleanProduct
+-- Simplify negative powers - this doesn't work with the current algos in cleanProduct so we'll do it here
 simplifyProd (Product (Power b e@(Number n)) t) | b == t && n < 0 = Power b (Sum e (Number 1))
 simplifyProd (Product t (Power b e@(Number n))) | b == t && n < 0 = Power b (Sum e (Number 1))
 simplifyProd (Product (Power b1 e1@(Number n1)) (Power b2 e2@(Number n2))) | b1 == b2 && (n1 < 0 || n2 < 0) = Power b1 (Sum e1 e2)
@@ -109,6 +110,7 @@ prodTermCompare :: SymTerm -> SymTerm -> Ordering
 prodTermCompare (Number n1) (Number n2) = n1 `compare` n2
 prodTermCompare (Variable v1) (Variable v2) = v1 `compare` v2
 prodTermCompare (Number n1) (Variable v1) = LT
+prodTermCompare (Variable v1) (Number n1) = GT
 prodTermCompare (Power b1 _) (Power b2 _) = b1 `prodTermCompare` b2
 prodTermCompare (Power b1 _) t | b1 == t = GT
                            | otherwise = b1 `prodTermCompare` t
@@ -130,12 +132,13 @@ listToProd [] = Number 1
 listToProd [t] = t
 listToProd (t:ts) = Product t (listToProd ts)
 
-prodListIntersectTuple :: Eq a => [a] -> [a] -> ([a],[a],[a])
+prodListIntersectTuple :: [SymTerm] -> [SymTerm] -> ([SymTerm],[SymTerm],[SymTerm])
 prodListIntersectTuple a b = let is = prodListIntersect a b in
                              (is, a \\ is, b \\ is)
 
 -- Data.List.intersect doesn't do what we need here
-prodListIntersect :: Eq a => [a] -> [a] -> [a]
+prodListIntersect :: [SymTerm] -> [SymTerm] -> [SymTerm]
+prodListIntersect ((Number n):xs) ys = prodListIntersect xs ys
 prodListIntersect (x:xs) ys = if x `elem` ys
                               then x:(prodListIntersect xs $ delete x ys)
                               else prodListIntersect xs ys
@@ -187,6 +190,8 @@ sumTermCompare (Number _) (Number _) = EQ
 sumTermCompare (Variable v1) (Variable v2) = v1 `compare` v2
 sumTermCompare (Number _) (Variable _) = LT
 sumTermCompare (Variable _) (Number _) = GT
+sumTermCompare (Variable v1) (Product (Number n) (Variable v2)) = v1 `compare` v2
+sumTermCompare (Product (Number n) (Variable v2)) (Variable v1)  = v2 `compare` v1
 sumTermCompare (Number _) t = LT
 sumTermCompare (Variable _) t = LT
 sumTermCompare t (Number _) = GT
@@ -195,13 +200,23 @@ sumTermCompare t1 t2 = EQ
 
 sumGroupable :: SymTerm -> SymTerm -> Bool
 sumGroupable (Number _) (Number _) = True
-sumGroupable (Variable _) (Variable _) = True
+sumGroupable (Variable a) (Variable b) = a == b
+sumGroupable (Variable a) (Product (Number n) (Variable b)) | a == b = True
+sumGroupable (Product (Number n) (Variable b)) (Variable a) | a == b = True
 sumGroupable (Product _ _) (Product _ _) = True
 sumGroupable _ _ = False
+
+--            New term   Accumulator
+consolidSum :: SymTerm -> SymTerm -> SymTerm
+consolidSum (Variable v1) (Variable v2) | v1 == v2 = Product (Number 2) (Variable v1)
+consolidSum (Variable v1) (Product (Number n) (Variable v2)) | v1 == v2 = Product (Number $ n+1) (Variable v1)
+consolidSum (Product (Number n) (Variable v2)) (Variable v1)  | v1 == v2 = Product (Number $ n+1) (Variable v1)
+consolidSum (Product (Number n1) t1) (Product (Number n2) t2) | t1 == t2 = Product (Number $ n1 + n2) t1
+consolidSum t1 t2 = Sum t1 t2
 
 -- Tidy up products
 cleanProduct :: SymTerm -> SymTerm
 cleanProduct p@(Product _ _) = listToProd . prodListToPowers {-. sortBy prodTermCompare // sorted in prodListToPowers-} . prodToList $ p
 
 cleanSum :: SymTerm -> SymTerm
-cleanSum s@(Sum _ _) = listToSum . map listToSum . groupBy sumGroupable . sortBy sumTermCompare . sumToList $ s
+cleanSum s@(Sum _ _) = listToSum . map (foldr1 consolidSum) . groupBy sumGroupable . sortBy sumTermCompare . sumToList $ s
