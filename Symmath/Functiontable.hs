358a7976bda745b39b9950178b3827de4cb73dd4
lbo <lbo@spheniscida.de>
2013-09-18 16:10:01 +0200

    Improved function table functions radically
    At this point, a rather good-looking function table can be printed with
    this call and looks like the below shown table:
    
    ghci> defaultFunctionTable 0 40 10 (Fraction 1 (Variable 'x'))
                  x       (1.0 / x)
    --------------- ---------------
             0.0000        Infinity
            10.0000          0.1000
            20.0000          0.0500
            30.0000          0.0333
            40.0000          0.0250
    
    We're also not using any third-party packages anymore for
    pretty-printing but the standard Text.PrettyPrint.

--- Symmath/Functiontable.hs
+++ Symmath/Functiontable.hs
@@ -13,21 +13,40 @@ import Symmath.Util
 
 data FuncValue = Value Double | Undefined
 
-
 instance Show FuncValue where
     show (Value n) = show n
     show Undefined = "undef"
 
--- Returns a list of Docs, representing a column.
--- Takes:       Start value, End value, Interval, field width, accuracy, term, independent variable
-funcToDocList :: Double -> Double -> Double -> Int -> Int -> SymTerm -> Char -> [Doc]
-funcToDocList from to ival width accur term indep = (text . printf ('%':show width++"s") . show $ term) : (text . printf ('%':show width++"s") . replicate width $ '-') : map (printfFuncValue width accur . calcFunc) [from,ival..to]
-    where calcFunc x = case evalTermP term [(indep,x)] of
-                            Just y -> Value y
-                            Nothing -> Undefined
+-- Use show on the returned Docs to format (render) them.
 
+
+------------ Functions using defaults --------------------
+-- Uses default values for field width, accuracy and the independent variable
 defaultFunctionTable :: Double -> Double -> Double -> SymTerm -> Doc
-defaultFunctionTable from to ival term = foldr1 ($$) $ funcToDocList from to ival 15 3 term 'x'
+defaultFunctionTable from to ival term = multipleFunctionsDefault from to ival [term]
+
+multipleFunctionsDefault :: Double -> Double -> Double -> [SymTerm] -> Doc
+multipleFunctionsDefault from to ival terms = multipleFunctions from to ival width acc terms indep
+    where width = 15
+          acc = 4
+          indep = 'x'
+
+
+----------- non-default-using functions -------------------
+
+functionTable :: Double -> Double -> Double -> Int -> Int -> SymTerm -> Char -> Doc
+functionTable from to ival width acc term indep = multipleFunctions from to ival width acc [term] indep
+
+multipleFunctions :: Double -> Double -> Double -> Int -> Int -> [SymTerm] -> Char -> Doc
+multipleFunctions from to ival width acc terms indep = foldr1 ($$) . (header:) . map (foldr1 (<+>) . calcLine) $ [from,ival..to]
+    where header = colHeads $$ headSepLine
+          colHeads = foldr1 (<+>) . map (text . printf ('%':show width++"s") . show) $ terms'
+          headSepLine = foldr1 (<+>) $ replicate (length terms') (text . replicate width $ '-')
+          calcLine x = map (calcFunc x) terms'
+          calcFunc x term = case evalTermP term [(indep,x)] of
+                            Just y -> printfFuncValue width acc $ Value y
+                            Nothing -> printfFuncValue width acc Undefined
+          terms' = (Variable indep):terms
 
 -- Util
 
