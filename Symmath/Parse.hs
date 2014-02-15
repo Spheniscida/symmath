Spheniscida/symmath

commit c7a0f143fee5b529688f024892326f0174afd01b

Make parser fail if input isn't consumed completely

Symmath/Parse.hs
@@ -15,7 +15,7 @@ parseStr :: String -> Maybe SymTerm
 parseStr = eitherToMaybe . parse expr ""
 
 expr :: SymParser
-expr = buildExpressionParser opTable term
+expr = buildExpressionParser opTable term <* eof
 
 opTable :: OperatorTable Char () SymTerm
 opTable = [[Infix (Power <$ char '^') AssocLeft]

commit d1885b4b668a4f19d49421d56aaa9efdfc5ee77e

Fix parser failure on parentheses
This bug was introduced with c7a0f143fee5b529688f024892326f0174afd01b.

Symmath/Parse.hs
@@ -17,7 +17,10 @@ parseStr :: String -> Maybe SymTerm
 parseStr = eitherToMaybe . parse expr ""
 
 expr :: SymParser
-expr = buildExpressionParser opTable term <* eof
+expr = subExpr <* eof
+
+subExpr :: SymParser
+subExpr = buildExpressionParser opTable term
 
 opTable :: OperatorTable Char () SymTerm
 opTable = [[Infix (Power <$ char '^') AssocLeft]
@@ -36,7 +39,7 @@ mathTerm = parens
    <|> num
 
 parens :: SymParser
-parens = char '(' *> expr <* char ')'
+parens = char '(' *> subExpr <* char ')'
 
 mathFun :: SymParser
 mathFun = funName <*> parens