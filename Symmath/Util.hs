22153aeff8d2c1206add8fd3975d3ed906df7cc0
lbo <lbo@spheniscida.de>
2014-02-15 22:37:01 +0100

    Improved Symmath.Util.elems and introduced Symmath.Util.(>><)
    (>><) "replaces" a value within a monad.

--- Symmath/Util.hs
+++ Symmath/Util.hs
@@ -5,12 +5,16 @@ import Data.List
 eitherToMaybe :: Either a b -> Maybe b
 eitherToMaybe = either (const Nothing) Just
 
+-- Checks if a double can be considered to be an integer (i.e., whether it is considerably different from its floor)
 isIntegral :: Double -> Bool
 isIntegral x = (x - (fromIntegral . floor $ x)) == 0
 
+-- Checks if all elements of the first list are contained in the second one
 elems :: Eq a => [a] -> [a] -> Bool
-elems [e] l = e `elem` l
-elems (e:es) l = case (e `elem` l) of
-                    True -> elems es (l \\ [e])
-                    False -> False
+elems [] l = True
+elems (e:es) l = (e `elem` l) && elems es (l \\ [e])
+
+-- Basically <$ for monads. <$ doesn't really work on the Parser monad
+(>><) :: Monad m => m a -> b -> m b
+m >>< b = m >> return b
 
